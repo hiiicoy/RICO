10903dd33838c7b88e5fe0980c71a508a2590f40
hiiicoy <calatravarico433@gmail.com>
2024-08-14 09:13:40 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<main> 
+    <form action="includes/formhandler.php"method="post">
+        <label for="firstname">Firstname</label>
+        <input id="firstname"type="text"name="firstname"placeholder="firstname">
+
+        <label for="lastname">lastname</label>
+        <input id="lastname"type="text"name="lastname"placeholder="lastname">
+         
+
+        <label for="gender">Gender</label>
+        <label id="gender"name="gender">
+        <label value="gender">None</option>
+        <label value="gender">Male</option>
+        <label value="gender">Female</option>
+         
+
+        /<select>
+        <button type="submit">Submit</button>
+</form>    
+</body>
+</html>
